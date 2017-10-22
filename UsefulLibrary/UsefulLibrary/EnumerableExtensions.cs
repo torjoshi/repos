f435ae8b4c8939670f7149dcf4f0e31d6e56a1f2
Rajesh Joshi <torjoshi@users.noreply.github.com>
2017-10-22 10:56:09 -0700

    Initial commit

---
+++ UsefulLibrary/UsefulLibrary/EnumerableExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UsefulLibrary
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Has more than one elements.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static bool HasMany<T>(this IEnumerable<T> source) => source?.Take(1).Any() ?? false;
+
+        /// <summary>
+        /// HasMany matching a predicate.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+
+        public static bool HasMany<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            //Add check for the predicate.
+            return HasMany(source.Where(e => predicate(e)));
+        }
+    }
+}
